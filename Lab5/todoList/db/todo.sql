bfe935c26a72b8302d338777133f70d02841efc9
Takarjn <tranlamiter@gmail.com>
2022-06-08 22:40:30 +0700

    update 5.0 todoList

---
+++ Lab5/todoList/db/todo.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE `items` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `item_name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ;
+
+INSERT INTO `items` VALUES(1, 'Get Milk');
+INSERT INTO `items` VALUES(2, 'Buy Application');
